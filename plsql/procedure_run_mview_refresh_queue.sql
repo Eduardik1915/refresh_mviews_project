5b9d80f7cca57d22d5824d2f1f6df9980acc8601
Eduards <Eduardik1915@gmail.com>
2025-06-02 09:21:17 +0300

    fixed loop issue

--- plsql/procedure_run_mview_refresh_queue.sql
+++ plsql/procedure_run_mview_refresh_queue.sql
@@ -1,45 +1,45 @@
-CREATE OR REPLACE procedure run_mview_refresh_queue AS
-    l_mview_name mview_refresh_queue.mview_name%TYPE;
+CREATE OR REPLACE PROCEDURE EDUARDS.run_mview_refresh_queue AS 
+	l_mview_name mview_refresh_queue.mview_name%TYPE := NULL;
 BEGIN
     LOOP
-
         BEGIN
-	        SELECT mview_name
-	        INTO l_mview_name
-	        FROM
-	           (SELECT mrq.mview_name
-	            FROM mview_refresh_queue mrq
-	            WHERE mrq.status = 'ready'
-	            AND NOT EXISTS (
-	                SELECT 1
-	                FROM mview_refresh_queue mv
-	                JOIN mview_dependency_map mdm
-	                ON mv.mview_name = mdm.depends_on
-	                WHERE mdm.mview_name = mrq.mview_name
-	                AND mv.status != 'done'
-	            )
-	            ORDER BY mrq.priority)
-	            WHERE rownum = 1
-	            FOR UPDATE SKIP LOCKED;
+            UPDATE mview_refresh_queue mrq
+            SET status = 'in progress'
+            WHERE mrq.mview_name = (
+                    SELECT mview_name
+                    FROM (
+                        SELECT mrq2.mview_name
+                        FROM mview_refresh_queue mrq2
+                        WHERE mrq2.status = 'ready'
+                          AND NOT EXISTS (
+                                SELECT 1
+                                FROM mview_refresh_queue mrq3
+                                JOIN mview_dependency_map mdm
+                                  ON mrq3.mview_name = mdm.depends_on
+                                WHERE mdm.mview_name = mrq2.mview_name
+                                  AND mrq3.status != 'done'
+                        )
+                        ORDER BY mrq2.priority
+                    )
+                    WHERE ROWNUM = 1
+            )
+            RETURNING mview_name INTO l_mview_name;
 
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
+            IF SQL%ROWCOUNT = 0 THEN
                 EXIT;
+            END IF;
+
+            COMMIT;
+
         END;
 
+        BEGIN
+            dbms_output.put_line ('refresing: ' || l_mview_name);
+            --dbms_mview.refresh(l_mview_name, METHOD => 'C', ATOMIC_REFRESH => FALSE);
         UPDATE mview_refresh_queue
-        SET status = 'in progress'
+        SET status = 'done'
         WHERE mview_name = l_mview_name;
 
-        COMMIT;
-
-        BEGIN
-            dbms_mview.refresh(l_mview_name, METHOD => 'C', ATOMIC_REFRESH => FALSE);
-
-            UPDATE mview_refresh_queue
-            SET status = 'done'
-            WHERE mview_name = l_mview_name;
-        
         EXCEPTION
             WHEN OTHERS THEN
                 UPDATE mview_refresh_queue
@@ -48,6 +48,7 @@ BEGIN
         END;
 
         COMMIT;
+
     END LOOP;
+
 END;
-     
